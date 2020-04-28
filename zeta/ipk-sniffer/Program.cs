3f640fa1cc822aad23f53c26464b5fbb41513705
hojkas <ivka.strnadova@gmail.com>
2020-04-28 23:44:04 +0200

    hello world

--- zeta/ipk-sniffer/Program.cs
+++ zeta/ipk-sniffer/Program.cs
@@ -7,6 +7,7 @@ namespace ipk_sniffer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I did something.");
         }
     }
 }
