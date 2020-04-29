12cd0818149e529ecfe95931326f5972e556d8f1
hojkas <ivka.strnadova@gmail.com>
2020-04-29 23:50:52 +0200

    sum stuff

--- zeta/ipk-sniffer/Program.cs
+++ zeta/ipk-sniffer/Program.cs
@@ -1,14 +1,54 @@
 ﻿using System;
+using PacketDotNet;
+using CommandLine;
 
-namespace ipk_sniffer
+class Argument
 {
-  class Program
+  public string rozhrani;
+  public int port;
+  public bool tcp;
+  public bool ucp;
+  public int num;
+
+  public Argument()
   {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");
-      Console.WriteLine("I did something.");
-      Console.WriteLine("Hey");
-    }
+    rozhrani = null;
+    tcp = false;
+    ucp = false;
+    num = 1;
+  }
+
+  public class Options
+  {
+    [Option('t', "tcp", Required = false, HelpText = "Analyze TCP packets.")]
+    public bool Tcp { get; set; }
+    [Option('u', "udp", Required = false, HelpText = "Analyze UDP packets.")]
+    public bool Udp { get; set; }
+  }
+
+  public void parse_arguments(string[] args)
+  {
+    string shortopts = "tui:p:n:";
+    Parser.Default.ParseArguments<Options>(args)
+      .WithParsed<Options>(o =>
+      {
+        if(o.Tcp) {
+          Console.WriteLine("TCP on");
+        }
+
+        if(o.Udp) {
+          Console.WriteLine("UDP on");
+        }
+
+      });
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Argument arg = new Argument();
+    arg.parse_arguments(args);
   }
 }
