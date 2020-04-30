dd78bedcc3dd73c3806253d3c1dba6f57cc24c63
hojkas <ivka.strnadova@gmail.com>
2020-04-30 17:57:29 +0200

    update, devices -i done

--- zeta/ipk-sniffer/Program.cs
+++ zeta/ipk-sniffer/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
-using PacketDotNet;
 using CommandLine;
+using SharpPcap;
+// ReSharper disable CommentTypo
 
 class Argument
 {
-  public int port;
-  public bool all_ports;
-  public int num;
-  public bool tcp;
-  public bool udp;
-  public string inter;
+  public int Port;
+  public bool AllPorts;
+  public int Num;
+  public bool Tcp;
+  public bool Udp;
+  public string Inter;
 
   public Argument()
   {
-    all_ports = false;
-    tcp = true;
-    udp = true;
+    AllPorts = false;
+    Tcp = true;
+    Udp = true;
   }
 
   public class Options
@@ -41,8 +42,21 @@ class Argument
       .WithParsed<Options>(o =>
       {
         //Kontrola zda chyběl přepínač -i
+        //V takovém případě načte aktivní zařízení, vypíše je a skončí program
         if(String.IsNullOrEmpty(o.Interface)) {
-          //TODO handle missing interface (print stuff)
+          //Načte zařízení do seznamu pro pozdější zpracování
+          CaptureDeviceList dev_list = CaptureDeviceList.Instance;
+          
+          if (dev_list.Count < 1) {
+            Console.WriteLine("No devices active on this machine.");
+            Environment.Exit(0);
+          }
+
+          Console.WriteLine("Active devices:\n-----------");
+          //Cyklus vypíše jména aktivních rozhraní
+          foreach (ICaptureDevice dev in dev_list) {
+            Console.WriteLine(dev.Name);
+          }
           Environment.Exit(0);
         }
 
@@ -51,37 +65,44 @@ class Argument
           Console.WriteLine("Invalid port number recieved (" + o.Port.ToString() + ").");
           Environment.Exit(1);
         }
-        if(o.Port == 0) all_ports = true;
+        if(o.Port == 0) AllPorts = true;
 
         //Překopírování hodnot do members této třídy
-        port = o.Port;
-        num = o.Num;
-        inter = o.Interface;
+        Port = o.Port;
+        Num = o.Num;
+        Inter = o.Interface;
 
         //Default hodnota tcp, udp je true, tímto se nastaví správně true/false
         //hodnoty (aby zadny prepinac/oba vyustily v obě hodnoty true)
-        if(!o.Tcp && o.Udp) tcp = false;
-        if(!o.Udp && o.Tcp) udp = false;
+        if(!o.Tcp && o.Udp) Tcp = false;
+        if(!o.Udp && o.Tcp) Udp = false;
       });
-
   }
 
   public void parse_debug()
   {
-    Console.WriteLine("Interface: " + inter);
-    Console.WriteLine("How many packet: " + num.ToString());
-    if(all_ports) Console.WriteLine("All ports");
-    else Console.WriteLine("Port: " + port.ToString());
-    if(tcp && udp) Console.WriteLine("TCP & UDP");
+    Console.WriteLine("Interface: " + Inter);
+    Console.WriteLine("How many packet: " + Num.ToString());
+    if(AllPorts) Console.WriteLine("All ports");
+    else Console.WriteLine("Port: " + Port.ToString());
+    if(Tcp && Udp) Console.WriteLine("TCP & UDP");
     else
-      if(tcp) Console.WriteLine("TCP only");
-      else if(udp) Console.WriteLine("UDP only");
+      if(Tcp) Console.WriteLine("TCP only");
+      else if(Udp) Console.WriteLine("UDP only");
       else Console.WriteLine("Shouldn't happen, no tcp/udp");
   }
 }
 
 class Program
 {
+  private void catch_packets(Argument arg)
+  {
+    
+  }
+  
+  /* Main
+   * Volá zpracování argumentů a poté funkci na chytání paketů
+   */
   static void Main(string[] args)
   {
     Argument arg = new Argument();
